d23dd425ed295cdab4cf38cdbf8ead5c9447fa2f
ABdullah1alsayed <51753417+ABdullah1alsayed@users.noreply.github.com>
2021-11-25 15:38:13 +0200

    Create object.js

---
+++ object.js
@@ -0,0 +1,8 @@
+let myInfo = {
+    name:'Abdullah',
+    age:26,
+    adress:'10th of Ramadan'
+}
+console.log(`my name is ${myInfo.name} and my age is ${myInfo.age} and i live in ${myInfo.adress}`);
+myInfo.age =  myInfo.age+27
+console.log(`my name is ${myInfo.name} and my age is ${myInfo.age} and i live in ${myInfo.adress}`);
